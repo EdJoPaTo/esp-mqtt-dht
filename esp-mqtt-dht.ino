e4206c5e27817f39dc0a8c916df18a1d4cef1dce
Edgar To <edjopato@gmail.com>
2019-01-18 10:07:57 +0100

    fix: use same type of int

--- esp-mqtt-dht.ino
+++ esp-mqtt-dht.ino
@@ -124,7 +124,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  for (int i = 0; i < length; i++) {
+  for (unsigned int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
