24df17bc3c04ad78de6b516762f0664694223042
Edgar To <edjopato@gmail.com>
2019-01-19 01:51:47 +0100

    feat: simplify mac address
    As it is an esp the first 3 numbers are not relevant.

--- esp-mqtt-dht.ino
+++ esp-mqtt-dht.ino
@@ -84,10 +84,8 @@ void setup() {
 String macToStr(const uint8_t* mac)
 {
   String result;
-  for (int i = 0; i < 6; ++i) {
+  for (int i = 3; i < 6; ++i) {
     result += String(mac[i], 16);
-    if (i < 5)
-      result += ':';
   }
   return result;
 }
