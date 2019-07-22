93d760162f8d5a84e52c8b02fe0a352d2dd29d34
Edgar Toll <edjopato@gmail.com>
2019-07-22 19:54:06 +0200

    feat: Update RSSI every 5 min

--- config.example.h
+++ config.example.h
@@ -15,7 +15,7 @@ const int DHTPIN = 12; // digital pin of the NodeMCU (D6 in this case)
 // hint: 1 measurement every 5 seconds, 12 measurements every minute
 const int SEND_EVERY_TEMP = 12 * 2; // send every 2 minutes
 const int SEND_EVERY_HUM = 12 * 5; // send every 5 minutes
-const int SEND_EVERY_RSSI = 12 * 15; // send every 15 minutes
+const int SEND_EVERY_RSSI = 12 * 5; // send every 5 minutes
 
 // Enable when you want to see the actual values published over MQTT after each measurement
 // #define DEBUG_KALMAN
