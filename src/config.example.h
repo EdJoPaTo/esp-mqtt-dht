613d918c8cef77ad7ac734abd3cbcc533caee28f
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-01-09 22:00:09 +0100

    docs: use different default PIN on ESP32

--- src/config.example.h
+++ src/config.example.h
@@ -8,7 +8,12 @@
 const bool MQTT_RETAINED = true;
 
 const bool IS_DHT11 = false;
-const int DHTPIN = 12; // digital pin of the NodeMCU (D6 in this case)
+
+#ifdef ESP8266
+  const int DHTPIN = 12; // digital pin of the NodeMCU v2 (D6 in this case)
+#else // for ESP32
+  const int DHTPIN = 15; // digital pin of the NodeMCU ESP32 (D15 in this case)
+#endif
 
 // hint: 1 measurement every 5 seconds, 12 measurements every minute
 const int SEND_EVERY_TEMP = 12 * 2; // send every 2 minutes
